022d3d99fb704dfffd09094171bb2b838aacc958
Jacqueline Roignant <jacqueline.andressa2408@gmail.com>
2026-05-19 23:09:44 +0200

    fix: enable React Router v7 future flags to remove console warnings

--- src/Router.jsx
+++ src/Router.jsx
@@ -8,7 +8,9 @@ import ScrollToTop from './components/ScrollToTop'
 
 function Router() {
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
       <ScrollToTop />
       <Routes>
         <Route
